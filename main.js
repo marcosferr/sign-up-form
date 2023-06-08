42aa932cf219f2727ebfb2351f6c67a52bac7eb6
marcosferr <81393592+marcosferr@users.noreply.github.com>
2023-06-07 21:38:28 -0400

    prevent default form submit

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 
 const campo1 = document.getElementById('password');
 const campo2 = document.getElementById('cpassword');
-
+const submitBtn = document.getElementById('submitBtn')
+submitBtn.addEventListener('click', (e)=> {
+e.preventDefault()
+alert('Datos ingresados correctamente!')
+})
 campo2.addEventListener("focusout", ()=>{
     if(campo1.value != campo2.value){
         alert("Contraseñas no coinciden")
