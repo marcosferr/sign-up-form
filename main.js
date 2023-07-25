8652311eac60112e5e99726c0a90387db4e8bbd6
marcosferr <ferreiramarcosadrian@gmail.com>
2023-07-25 12:59:38 -0400

    Add javascript basic validation

--- main.js
+++ main.js
@@ -1,13 +1,54 @@
+const campo1 = document.getElementById("password");
+const campo2 = document.getElementById("cpassword");
+const email = document.getElementById("email");
+const nameField = document.getElementById("name");
+const lastName = document.getElementById("lastname");
+const phone = document.getElementById("phone");
 
-const campo1 = document.getElementById('password');
-const campo2 = document.getElementById('cpassword');
-const submitBtn = document.getElementById('submitBtn')
-submitBtn.addEventListener('click', (e)=> {
-e.preventDefault()
-alert('Datos ingresados correctamente!')
-})
-campo2.addEventListener("focusout", ()=>{
-    if(campo1.value != campo2.value){
-        alert("Contraseñas no coinciden")
-    }
+const submitBtn = document.getElementById("submitBtn");
+const form = document.getElementById("submit");
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (
+    campo1.checkValidity() &&
+    campo2.checkValidity() &&
+    nameField.checkValidity() &&
+    lastName.checkValidity() &&
+    phone.checkValidity() &&
+    checkPassword()
+  ) {
+    alert("Datos ingresados correctamente!");
+  } else {
+    alert("Rellene los campos correctamente");
+  }
+});
+
+function checkPassword() {
+  if (campo1.value !== campo2.value) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+campo2.addEventListener("focusout", () => {
+  if (checkPassword()) {
+    alert("Contraseñas no coinciden");
+  }
+});
+
+email.addEventListener("change", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+phone.addEventListener("change", (event) => {
+  if (phone.validity.typeMismatch) {
+    phone.setCustomValidity("I am expecting a phone number");
+  } else {
+    phone.setCustomValidity("");
+  }
 });
